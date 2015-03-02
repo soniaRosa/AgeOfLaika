542d3d98a9bb7ca18bc973b17a55e1a2cceb525c
Joao Rosa <joaorosa@Air-de-Joao.lan>
2015-03-02 21:27:52 -0100

    first commit

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var dogAgeInHumanYearsLabel: UILabel!
+    @IBOutlet weak var humanAgeInDogYearsLabel: UILabel!
 
+    @IBOutlet weak var dogYearsTextField: UITextField!
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,72 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - IBAction
+    @IBAction func convertYearsButtonPressed(sender: UIButton) {
+        
+        convertDogYearsInHumanYears()
+        ConvertHumanYearsInDogYears()
+    }
+    @IBAction func tryAgainButtonPressed(sender: UIButton) {
+        cleanAndTryAgain()
+    }
+    
+    // MARK: - Helper Functions
+    func convertDogYearsInHumanYears () {
+        
+        let stringFromTextField = dogYearsTextField.text
+        let stringConvertfromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+        var realDogAgeInHumanYears: Double
+        
+        if stringConvertfromTextField <= 2 {
+            
+            realDogAgeInHumanYears = stringConvertfromTextField * 10.5
+            dogAgeInHumanYearsLabel.hidden = false
+        }
+        else {
+            
+            realDogAgeInHumanYears = (10.5 * 2) + (stringConvertfromTextField - 2) * 4
+            dogAgeInHumanYearsLabel.hidden = false
+        }
+        
+        dogAgeInHumanYearsLabel.text =  " Your dog have \(realDogAgeInHumanYears) in human years"
+        dogYearsTextField.resignFirstResponder()
+    }
+    
+    func ConvertHumanYearsInDogYears () {
+        let stringFromTextField = humanYearsTextField.text
+        let stringConvertFromTextField = Double((stringFromTextField as NSString).doubleValue)
+        
+        var realHumangeInDogYears: Double
+        
+        if stringConvertFromTextField <= 21 {
+            
+            realHumangeInDogYears = stringConvertFromTextField / 10.5
+            humanAgeInDogYearsLabel.hidden = false
+        }
+        else {
+            realHumangeInDogYears = (21 / 10.5) + (stringConvertFromTextField - 21) / 4
+            humanAgeInDogYearsLabel.hidden = false
+        }
+        humanAgeInDogYearsLabel.text = " You have \(realHumangeInDogYears) in dog years"
+        humanYearsTextField.resignFirstResponder()
+    }
+    
+    func cleanAndTryAgain() {
+        
+        dogAgeInHumanYearsLabel.hidden = true
+        dogYearsTextField.text = ""
+        
+        humanAgeInDogYearsLabel.hidden = true
+        humanYearsTextField.text = ""
 
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
 }
 
